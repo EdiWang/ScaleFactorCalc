eed57f15324149d0dee8b90a068cee5f07c2030a
Edi Wang <Edi.Wang@outlook.com>
2023-10-20 09:31:02 +0800

    empty fluent blazor app

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using src.Components;
+using Microsoft.Fast.Components.FluentUI;
+using ScaleFactorCalc.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFluentUIComponents(options =>
+{
+    options.HostingModel = BlazorHostingModel.WebAssembly;
+});
 
 await builder.Build().RunAsync();
